9f9688a06903e8c7e0101b9cdf9831342b6eb9ef
xiaokinglong <wang_jin_ong@163.com>
2022-04-28 11:56:06 +0800

    基础布局

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "ant-design-vue/dist/antd.css";
 import naive from 'naive-ui'
 
 createApp(App).use(naive).use(router).mount("#app");
